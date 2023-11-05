d67d5b8edcacb0f19e4efb42b4ad75a70d94ed45
Marius Ehret <ehret.marius@gmail.com>
2023-11-05 13:22:37 +0100

    added tests for hints and examples

--- end2end-test.py
+++ end2end-test.py
@@ -4,19 +4,47 @@ Copyright 2023, University of Freiburg,
 Chair of Algorithms and Data Structures
 Author: Hannah Bast <bast@cs.uni-freiburg.de>
 """
+import json
+import time
+import re
+import os
 
-from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 # from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 import argparse
 import logging
 import sys
+
+
+class Hint:
+    def __init__(self, text):
+        self.fulltext = text
+        self.database_type, self.database_id, self.name_language_pairs = self.parse(text)
+        self.primary_name = self.name_language_pairs[0][0]
+
+    def parse(self, text):
+        database_type, database_id = None, None
+        name_language_pairs = []
+
+        parts = text.split('/')
+        for part in parts:
+            elements = re.split(r'["@]+', part.strip())
+            if not database_type:
+                database_type, database_id = re.search(r'(\w+):([A-Z]\d+)', elements[0]).groups()
+            name = elements[1]
+            language = elements[2]
+            name_language_pairs.append((name, language))
+
+        return database_type, database_id, name_language_pairs
+
+    def get_names(self):
+        return [pair[0] for pair in self.name_language_pairs]
 
 
 # Global log with custom formatter, inspired by several posts on Stackoverflow.
@@ -53,7 +81,7 @@ class QleverUiTester:
     https://github.com/ad-freiburg/hisinone-scraper
     """
 
-    def __init__(self, headless, url, num_retries):
+    def __init__(self, headless, url, test_case_path, num_retries):
         """
         Basic settings and open the browser window.
         """
@@ -62,6 +90,11 @@ class QleverUiTester:
         self.url = url
         self.num_retries = num_retries
         self.timeout_loading = 5
+
+        self.test_cases_hints = []
+        self.test_cases_examples = []
+        self.init_test_cases(test_case_path)
+
         options = Options()
         if self.headless:
             log.info("Running in \x1b[1mheadless\x1b[0m mode")
@@ -79,22 +112,28 @@ class QleverUiTester:
         """
         Close the browser window if it's still there.
         """
-
+        input()
+        log.info("Shutting down...")
         try:
             self.driver.close()
         except Exception:
             pass
 
-    def test(self):
-        """
-        Some basic tests to check if the UI is working.
-        """
+    def init_test_cases(self, test_case_path):
+        try:
+            with open(test_case_path, 'r') as file:
+                data = json.load(file)
+                self.test_cases_hints = data.get("Testcases_Hints", [])
+                self.test_cases_examples = data.get("Testcases_Examples", [])
+        except:
+            log.error("Could not initialize test cases.")
 
+    def init_page(self):
         for i in range(self.num_retries):
             try:
                 self.driver.get(self.url)
                 WebDriverWait(self.driver, self.timeout_loading).until(
-                      EC.presence_of_element_located((By.ID, "query")))
+                    EC.presence_of_element_located((By.ID, "query")))
                 log.info(f"Page {self.url} loaded successfully")
                 break
             except Exception as e:
@@ -107,6 +146,119 @@ class QleverUiTester:
                     log.error("Aborting after %d retries." % self.num_retries)
                     self.done()
                     sys.exit(1)
+
+    def send_to_textfield(self, text):
+        textfield = self.driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/div/div[1]/div/div[1]/textarea")
+        textfield.send_keys(text)
+
+    def get_hints(self):
+        for i in range(10):
+            # wait until there are hints not starting with ? and then return them
+            # alternative: get all network requests and wait till all have a responseEnd (are answered)
+            #network_requests = self.driver.execute_script("var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;")
+
+            time.sleep(0.5)
+            try:
+                hint_window = self.driver.find_element(By.XPATH, "/html/body/ul")
+            except:
+                log.info("Waiting for autocompletion hints...")
+                continue
+
+            hints = hint_window.find_elements(By.CLASS_NAME, "CodeMirror-hint")
+
+            for hint in hints:
+                if hint.text[0] != "?":
+                    return hints
+        log.info("No hints were shown")
+        return []
+
+    def test(self):
+        """
+        Running through different ui tests
+        """
+        self.test_examples()
+        self.test_hints()
+
+    def test_examples(self):
+        """
+        Testing the "Examples" function
+        """
+        for test_case in self.test_cases_examples:
+            log.info("----------")
+            log.info("Starting with Example Test %s" % test_case.get('name'))
+            self.init_page()
+
+            examples_button = self.driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/div/div[3]/div[2]/button[4]")
+            if not examples_button:
+                log.warning("Test Case %s: Could not find the examples button" % test_case.get('name'))
+                log.warning("Test Case %s: Testing examples button failed" % test_case.get('name'))
+                continue
+            examples_button.click()
+
+            dropdown_menu = self.driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/div/div[3]/div[2]/ul")
+            if not dropdown_menu or not dropdown_menu.is_displayed():
+                log.warning("Test Case %s: No examples were shown" % test_case.get('name'))
+                log.warning("Test Case %s: Testing examples button failed" % test_case.get('name'))
+                continue
+
+            example = dropdown_menu.find_element(By.XPATH, "//span[contains(text(), '%s')]" % test_case.get('input'))
+            if not example:
+                log.warning('Test Case %s: Did not find "%s" example' % (test_case.get('name'), test_case.get('input')))
+                log.warning("Test Case %s: Testing examples button failed" % test_case.get('name'))
+                continue
+            example.click()
+
+            text = self.driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/div/div[1]/div/div[6]/div[1]/div/div/div/div[5]")
+            lines = text.find_elements(By.XPATH, "//span[@role]")
+            expected_output = test_case.get('output').get('lines')
+            for i in range(len(expected_output)):
+                if lines[i].text != expected_output[i]:
+                    log.warning('Test Case %s (line %s/%s): displayed\n"%s"\ninstead of\n"%s"' %
+                                (test_case.get('name'), i + 1, len(expected_output), lines[i].text, expected_output[i]))
+                    log.warning('Test Case %s failed' % test_case.get('name'))
+                    break
+            else:
+                log.info('Test Case %s finished successfully')
+
+
+    def test_hints(self):
+        """
+        Running through different hint tests.
+        """
+        for test_case in self.test_cases_hints:
+            log.info("----------")
+            log.info("Starting with Hint Test %s" % test_case.get('name'))
+            self.init_page()
+            self.send_to_textfield(test_case.get('input'))
+            unprocessed_hints = self.get_hints()
+
+            hints = [Hint(unprocessed_hint.text) for unprocessed_hint in unprocessed_hints]
+            expected_hints = test_case.get('output').get('hints')
+            hints_found = []
+            hints_not_found = []
+
+            #for hint in hints:
+            #    print('["%s", "%s"],' % (hint.database_id, hint.primary_name))
+
+            for i in range(len(expected_hints)):
+                if hints[i].database_id in expected_hints[i]:
+                    hints_found.append(hints[i])
+                    log.info('Test Case %s (%s/%s): Hint %s "%s" displayed correctly' %
+                             (test_case.get('name'), i + 1, len(expected_hints),
+                              hints[i].database_id, hints[i].primary_name))
+                else:
+                    hints_not_found.append(hints[i])
+                    log.warning('Test Case %s (%s/%s): Hint %s "%s" displayed instead of %s "%s"' %
+                                (test_case.get('name'), i + 1, len(expected_hints),
+                                 hints[i].database_id, hints[i].primary_name,
+                                 expected_hints[i][0], expected_hints[i][1]))
+
+            if hints_not_found:
+                log.warning('Test Case %s: %s tests failed' % (test_case.get('name'), len(hints_not_found)))
+            else:
+                log.info('Test Case %s: completed successfully' % test_case.get('name'))
+
+        log.info("All Test Cases are finished.")
 
 
 class MyArgumentParser(argparse.ArgumentParser):
@@ -125,7 +277,7 @@ if __name__ == "__main__":
 
     # Setup parser and basic usage information.
     parser = MyArgumentParser(
-            epilog="Example invocation: python3 hisinone-scraper",
+            epilog="Example invocation: python3 qlever-end2end",
             formatter_class=argparse.RawDescriptionHelpFormatter)
 
     # Command line arguments.
@@ -136,6 +288,10 @@ if __name__ == "__main__":
             "--url", dest="url", type=str,
             default="https://qlever.cs.uni-freiburg.de",
             help="The URL of the QLever UI (may redirect)")
+    parser.add_argument(
+        "--test-case-path", dest="test_case_path", type=str,
+        default="end2end-testcases.json",
+        help="A path to a JSON File with the autocompletion testcases to check")
     parser.add_argument(
             "--num-retries", dest="num_retries", type=int, default=5,
             help="Number of retries for loading a page")
@@ -151,7 +307,6 @@ if __name__ == "__main__":
     log.info("Log level is \x1b[1m%s\x1b[0m" % args.log_level)
 
     # Test the QLever UI.
-    qleverui_tester = QleverUiTester(
-            not args.not_headless, args.url, args.num_retries)
+    qleverui_tester = QleverUiTester(not args.not_headless, args.url, args.test_case_path, args.num_retries)
     qleverui_tester.test()
     qleverui_tester.done()
